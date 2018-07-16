067da7807ea912df560b099a6e67396ec4eddfb8
junbo <shadow1163>
2018-07-16 10:53:35 +0800

    update xvfb & wgaprf

--- vnc-xvfb/entrypoint.sh
+++ vnc-xvfb/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # entrypoint.sh file for starting the xvfb with better screen resolution, configuring and running the vnc server, pulling the code from git and then running the test.
 export DISPLAY=:20
-Xvfb :20 -screen 0 1024x768x24 -nolisten tcp &
-x11vnc -storepasswd password /tmp/vncpass 
-x11vnc -rfbport 5920 -rfbauth /tmp/vncpass -display $DISPLAY -forever &
+Xvfb :20 -screen 0 1366x768x16 -nolisten tcp &
+sleep 5
+#x11vnc -storepasswd password /tmp/vncpass 
+x11vnc -rfbport 5920 -nopw -ncache 1 -display $DISPLAY -forever &
+cd /root/noVNC && ln -s vnc_lite.html index.html && ./utils/launch.sh --vnc localhost:5920 &
 startxfce4
 #Xvfb :20 -screen 0 1366x768x16 &
 #x11vnc -passwd TestVNC -display :20 -N -forever
